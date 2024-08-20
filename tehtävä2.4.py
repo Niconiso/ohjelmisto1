14674db516687d29115fb103c9b6e0bb26016151
Nicojo <nico.jokinen96@gmail.com>
2024-08-20 12:53:25 +0300

    Add files via upload

---
+++ tehtävä2.4.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Anna ensimmäinen kokonaisluku: "))
+numero2 = int(input("Anna toinen kokonaisluku: "))
+print("Numeroiden summa on:" )
+print(numero1 + numero2)
+print("Numeroiden tulo on:" )
+print(numero1 * numero2)
+print("Numeroiden keskiarvo on: ")
+print((numero1 + numero2) / 2)
