5b7b6e8d9e5ef4f9a79dd8896ea709f71f8c95e0
Niconiso <nico.jokinen96@gmail.com>
2024-08-26 09:23:06 +0300

    update

--- mod3/3.3.py
+++ mod3/3.3.py
@@ -1,6 +1,17 @@
-Bsukupuoli = input("Mikä on biologinen sukupuolesi: ")
+Bsukupuoli = input("Mikä on biologinen sukupuolesi (mies/nainen): ")
 HemoG = float(input("Sekä hemoglobiiniarvosi (g/l): "))
 
-if Bsukupuoli = "Nainen"
+if Bsukupuoli == "nainen":
+    alhainen = 114
+    korkea = 155
 
+elif Bsukupuoli == "mies":
+    alhainen = 134
+    korkea = 167
 
+if HemoG < alhainen:
+    print("Hemoglobiiniarvosi on alhainen.")
+elif (HemoG > korkea):
+    print("Hemoglobiiniarvosi on korkea.")
+else:
+    print("Hemoglobiiniarvosi ovat normaalit.")
