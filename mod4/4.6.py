47549cae1c88ca52dbdd6716a92f8a278f9405a0
Niconiso <nico.jokinen96@gmail.com>
2024-08-28 10:08:00 +0300

    update

---
+++ mod4/4.6.py
@@ -0,0 +1,15 @@
+import random
+
+pisteet = int(input("Anna arvottavien pisteiden määrä: "))
+ympyränS = 0
+i = 0
+
+for _ in range(pisteet):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x**2 + y**2 < 1:
+        ympyränS += 1
+    i += 1
+
+PiLiki = 4 * ympyränS / pisteet
+print(f"Piin likiarvo on : {PiLiki}")
