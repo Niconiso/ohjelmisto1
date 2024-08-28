50e642a6348291b4ff25c22f525086efaf7b5dbc
Niconiso <nico.jokinen96@gmail.com>
2024-08-28 10:21:50 +0300

    update

--- mod4/4.6.py
+++ mod4/4.6.py
@@ -4,7 +4,7 @@ pisteet = int(input("Anna arvottavien pisteiden määrä: "))
 ympyränS = 0
 i = 0
 
-for _ in range(pisteet):
+while i < pisteet:
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     if x**2 + y**2 < 1:
