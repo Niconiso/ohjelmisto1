00c4b5641afa7fbea140f441d446ccb2dac0fe5c
Niconiso <nico.jokinen96@gmail.com>
2024-09-03 15:25:15 +0300

    update

---
+++ mod6/6.5.py
@@ -0,0 +1,7 @@
+def parittomat(kokolista):
+    lista = [luku for luku in kokolista if luku % 2 == 0]
+    return lista
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Lista2 = parittomat(lista1)
+print("Alkuperäinen lista:", lista1)
+print("Parillinen lista:", Lista2)
