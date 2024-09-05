55e31b9fba758042ce3ef3f4d076661771ccee65
Niconiso <nico.jokinen96@gmail.com>
2024-09-05 13:10:01 +0300

    update

--- mod6/6.5.py
+++ mod6/6.5.py
@@ -1,5 +1,6 @@
 def parilliset(kokolista):
-    lista = [luku for luku in kokolista if luku % 2 == 0]
+    lista = [luku for luku in kokolista
+             if luku % 2 == 0]
     return lista
 lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Lista2 = parilliset(lista1)
